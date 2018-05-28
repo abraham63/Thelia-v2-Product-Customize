dfdf96082de40128da48394b11963e222cff631e
Kevin <kevincbar@gmail.com>
2018-05-28 11:47:59 +0200

    Update PersoProduitCommandeQuery.php

--- Model/PersoProduitCommandeQuery.php
+++ Model/PersoProduitCommandeQuery.php
@@ -5,16 +5,6 @@ namespace PersoProduit\Model;
 use PersoProduit\Model\Base\PersoProduitCommandeQuery as BasePersoProduitCommandeQuery;
 
 
-/**
- * Skeleton subclass for performing query and update operations on the 'order_comment' table.
- *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
- */
 class PersoProduitCommandeQuery extends BasePersoProduitCommandeQuery
 {
 
